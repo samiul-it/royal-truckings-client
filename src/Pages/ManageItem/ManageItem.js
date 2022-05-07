0124415f4ae7d0bcf9337b687eb5b002b6dc90e2
Samiul Islam <59833987+samiul1998@users.noreply.github.com>
2022-05-07 11:11:49 +0600

    Item Detail Added

--- src/Pages/ManageItem/ManageItem.js
+++ src/Pages/ManageItem/ManageItem.js
@@ -1,30 +1,77 @@
 import React, { useState } from 'react';
+import './ManageItem.css';
 
 import useItemDetail from './../../hooks/useItemDetails';
 import { useParams } from 'react-router-dom';
 import useItems from '../../hooks/useItems';
 import { useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 
 const ManageItem = () => {
     const {id}=useParams();
-    
-    // const [item,setItem]=useState({});
-    // useEffect(()=>{
-    //     const url = `http://localhost:5000/item/${id}`;
-    //     fetch(url)
-    //     .then(res=>res.json())
-    //     .then(data=>setItem(data));
-    // },[id])
+    const [itemDetail] = useItemDetail(id);
+    // const currentItems=itemDetail.quantity;
+    const [deliver,setDeliver]=useState();
+    const [stocOut,setStockOut]=useState(false);
 
-    const [itemDetail]=useItemDetail(id);
+    useEffect(()=>{
+        console.log("Current Stock:", deliver);
+        setDeliver(itemDetail.quantity);
+        if (itemDetail.quantity  === 0) {
+          console.log("Stock Out");
+          setStockOut(true);
+        }
+    },[itemDetail])
+
+
+    const deliverItem=()=>{
+        
+        
+        if(deliver>0){
+            const updateQty=deliver-1;
+            setDeliver(updateQty);
+            toast.success("1 Item Delivered!");
+        
+        }
+        else{
+            toast.error("Stock Out");
+            setStockOut(true);
+        }
+        
+    }
+    
+    
+
+    
     
     
     return (
-        <div>
-            <h1>Manage This Item:{id}</h1>
-            <h2>{itemDetail.productName}</h2>
+      <div>
+
+        <div className="card-container">
+          <div className="card-img">
+            <img src={itemDetail.img} alt="product-img" />
+          </div>
+          <div className="card-description">
+            <h5>{itemDetail.productName}</h5>
+            <h3 className="text-danger">${itemDetail.price}</h3>
+            <p>{itemDetail.description}</p>
+            <p>Supplier: {itemDetail.supplierName}</p>
+            <h6>Stock:{deliver}</h6>
+            {
+                stocOut? <h6 className='text-danger btn-link'>Stock Out</h6> :
+                <></>
+            }
+          </div>
+          <button
+            onClick={() => deliverItem(id)}
+            className=" btn btn-info  btn-block"
+          >
+            Deliver Item
+          </button>
         </div>
+      </div>
     );
 };
 
